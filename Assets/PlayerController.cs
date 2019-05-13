92991d84b67b2cefead279fe53bcc962b9074618
mahogany eggplant <ym081014@gmail.com>
2019-05-14 08:43:27 +0900

    タワーディフェンス
    バックアップ用

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//キャラクター操作
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (-0.02f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (0.02f, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (0, 0.02f, 0);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (0, -0.02f, 0);
+		}
+	}
+}
---
+++ Assets/enemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+	public Transform target;
+	public float speed = 0.001f;
+	private Vector3 vec;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//target = GameObject.Find("対象").transform;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), 0.05f);
+		transform.position += transform.forward * speed;
+
+		// 当たり判定
+		Vector2 p1=transform.position;
+		Vector2 p2 = this.target.transform.position;
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.8f;
+		float r2 = 0.8f;
+
+		if(d<r1+r2){
+			Debug.Log ("当たった");
+		}
+
+	}
+}
