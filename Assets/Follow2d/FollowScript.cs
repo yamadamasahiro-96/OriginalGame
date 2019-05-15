8522c6332aa5461bd0310a1608d4acd56bc850c8
mahogany eggplant <ym081014@gmail.com>
2019-05-15 14:34:15 +0900

    敵がプレイヤーを追いかけるように修正

---
+++ Assets/Follow2d/FollowScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FollowScript : MonoBehaviour
+{
+    [SerializeField] float m_moveSpeed = 1f;
+    [SerializeField] Transform m_target;
+    Rigidbody2D m_rb;
+
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 dir = m_target.position - transform.position;
+        dir.Normalize();
+        m_rb.velocity = dir * m_moveSpeed;
+    }
+}
--- Assets/enemyController.cs
+++ Assets/enemyController.cs
@@ -2,31 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class enemyController : MonoBehaviour
 {
-	public Transform target;
-	public float speed = 0.001f;
-	private Vector3 vec;
+	[SerializeField] float moveSpeed = 1f;
+	[SerializeField] Transform target;
+	Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start ()
 	{
 		//target = GameObject.Find("対象").transform;
+		rb = GetComponent<Rigidbody2D>();
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (target.position - transform.position), 0.05f);
-		transform.position += transform.forward * speed;
+		Vector2 dir = target.position - transform.position;
+		dir.Normalize();
+		rb.velocity = dir * moveSpeed;
 
 		// 当たり判定
 		Vector2 p1=transform.position;
 		Vector2 p2 = this.target.transform.position;
-		Vector2 dir = p1 - p2;
-		float d = dir.magnitude;
-		float r1 = 0.8f;
-		float r2 = 0.8f;
+		Vector2 dis = p1 - p2;
+		float d = dis.magnitude;
+		float r1 = 0.7f;
+		float r2 = 0.7f;
 
 		if(d<r1+r2){
 			Debug.Log ("当たった");
